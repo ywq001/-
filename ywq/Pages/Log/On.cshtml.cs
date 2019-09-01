dd6a7f16332082d58eb8e124cc64c6ae092a311d
Administrator <Administrator@5I1ZFT9ZO2GAJV7>
2019-09-01 16:41:44 +0800

    完成登录页面

---
+++ ywq/Pages/Log/On.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ywq.Pages.Log
+{
+    public class OnModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
