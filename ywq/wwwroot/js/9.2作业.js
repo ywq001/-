d5d5ab6f816360f079d56b8549f9b7d48bd04f3d
Administrator <Administrator@5I1ZFT9ZO2GAJV7>
2019-09-03 14:54:25 +0800

    9月2日作业完成一部分

---
+++ ywq/wwwroot/js/9.2作业.js
@@ -0,0 +1,11 @@
+﻿var sum = function () {
+    var j = 0, i = 0;
+    while (j < 100) {
+        j = j + 5;
+        i = i + j;
+    }
+    console.log(i);
+}
+
+var studens = ["彭志强", "于维谦", "兴龙泰", "薛明林", "陈百万", "杨进文", "王新", "陈小丁", "陈晨"];
+console.log(studens, studens.length);
