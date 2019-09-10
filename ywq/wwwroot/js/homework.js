264fbafebccc3dfc7825c769cbb0d291d292c81d
Administrator <Administrator@5I1ZFT9ZO2GAJV7>
2019-09-10 20:39:50 +0800

    完成构建函数has9/has8/has6/

--- ywq/wwwroot/js/homework.js
+++ ywq/wwwroot/js/homework.js
@@ -67,9 +67,7 @@
 //    for (var i = 0; i < flame.length; i++) {
 //        for (var j = 0; j < flame.length; j++) {
 //            if (flame[j] > flame[j + 1]) {
-//                //return function () {
-//                //    return j;
-//                //}
+//                
 //                Swap(flame, j, j + 1);
 //            }
 //        }
@@ -92,10 +90,10 @@
 
 //删除一个数组里面重复的元素
 //function element(method) {
-    //var arr = [];
-    //for (var i = 0; i < method.length; i++) {
-        //if (arr.indexOf(method[i]) == -1) {
-            //arr.push(method[i])
+//var arr = [];
+//for (var i = 0; i < method.length; i++) {
+//if (arr.indexOf(method[i]) == -1) {
+//arr.push(method[i])
 //        }
 //    }
 
@@ -123,7 +121,7 @@
 //        arr[i] = sept[i].toLowerCase().slice(0, 6);
 //        arr[i] = arr[i][0].toUpperCase() + arr[i].substring(1, 6);
 //    }
-    
+
 //    return arr;
 //}
 //var sept = ['why', 'gIT', 'vs2019', 'community', 'VERSION'];
@@ -183,10 +181,32 @@
 
 
 
-//在函数yz.fei.get986()中嵌入不带参数的函数has9() / has8() / has6() ，
-//并调用上述函数，找出10000以内有多少个数字包含：9或者8或者6。
+//构建一个函数has9(number) ，可以判断number中是否带有数字9；
+/*
+ * has9(8) => false
+ * has9(9) => true
+ * has9(69) =>true
+ * has9(91) =>true
+ * has9(-9.9) =>false
+ * has9(909) => true
+ * has9(9月9) =>false
+ */
+function has9(number) {
+    return hasX(number, 9);
+}
+
+function has8(number) {
+    return hasX(number, 8);
+}
+
+function has6(number) {
+    return hasX(number, 6);
+}
 
 
 
+function hasX(number, x) {
+    return String(number).indexOf(x) >= 0;
+}
 
-
+//使用上述函数，找出10000以内有多少个数字包含：9或者8或者6。
---
+++ ywq/wwwroot/js/homework9.9.js
@@ -0,0 +1,29 @@
+﻿//构建一个函数has9(number) ，可以判断number中是否带有数字9；
+/*
+ * has9(8) => false
+ * has9(9) => true
+ * has9(69) =>true
+ * has9(91) =>true
+ * has9(-9.9) =>false
+ * has9(909) => true
+ * has9(9月9) =>false
+ */
+function has9(number) {
+    return hasX(number, 9);
+}
+
+function has8(number) {
+    return hasX(number, 8);
+}
+
+function has6(number) {
+    return hasX(number, 6);
+}
+
+
+
+function hasX(number, x) {
+    return String(number).indexOf(x) >= 0;
+}
+
+//使用上述函数，找出10000以内有多少个数字包含：9或者8或者6。
