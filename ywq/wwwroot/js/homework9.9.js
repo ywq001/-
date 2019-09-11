668719591b86f664116f94679fe8749ec8cd50a8
Administrator <Administrator@5I1ZFT9ZO2GAJV7>
2019-09-11 22:01:56 +0800

    完成模拟名称空间部分

--- ywq/wwwroot/js/homework9.9.js
+++ ywq/wwwroot/js/homework9.9.js
@@ -1,4 +1,24 @@
-﻿//构建一个函数has9(number) ，可以判断number中是否带有数字9；
+﻿//使用“模拟名称空间”技术，构建一个函数函数yz.fei.get986(number)；
+//在yz.fei.get986(number)中包含不带参数的函数has9() / has8() / has6() ；
+//让get986()通过调用has9()/has8()/has6()，能找出number以内有多少个数字包含：9或者8或6。
+var global = overall || {};
+overall.yz.fei.get986 = function (number) {
+    var result = 0;
+    for (var i = 0; i < number + 1; i++) {
+        if (has9(i) || has8(i) || has6(i)) {
+            result++;
+        } else {
+            //什么都不做
+        }
+    }
+    console.log(result);
+}
+
+
+
+
+
+//构建一个函数has9(number) ，可以判断number中是否带有数字9；
 /*
  * has9(8) => false
  * has9(9) => true
@@ -27,3 +47,21 @@ function hasX(number, x) {
 }
 
 //使用上述函数，找出10000以内有多少个数字包含：9或者8或者6。
+/*测试
+ * 输入has（20）=>6
+ * 输入has（14）=>3
+ * 输入has（18）=>5
+*/
+
+function has(number) {
+    var result = 0;
+    for (var i = 0; i < number + 1; i++) {
+        if (has9(i) || has8(i) || has6(i)) {
+            result++;
+        } else {
+            //什么都不做
+        }
+    }
+    console.log(result);
+}
+has(20);
