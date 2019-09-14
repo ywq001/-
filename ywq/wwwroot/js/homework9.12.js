319ae7476a686de2a921bd79d82dbf9496ff6123
Administrator <Administrator@5I1ZFT9ZO2GAJV7>
2019-09-14 20:03:36 +0800

    完成使用setTimeout实现逐级递加每周上课人数

---
+++ ywq/wwwroot/js/homework9.12.js
@@ -0,0 +1,19 @@
+﻿//使用setTimeout() （不是setInterval() ）实现每隔1秒钟依次显示：第n周，源栈同学random人。（n逐次递增，random随机）
+//测试结果：每隔一秒显示，第n周，n逐次递加，直到第10周停止，上课人数小于20，不等于0.
+//var counter = 1;
+//function lesson() {
+//    if (counter < 10) {
+//        setTimeout(function () {
+//            console.log("第" + counter + "周,源栈同学" +
+//                Math.ceil(Math.random() * 20)+ "人");
+//            counter++;
+//            lesson();
+//        },1000);
+//    } else {
+//        //什么都不做
+//    };
+//};
+//lesson();
+//Math.ceil();
+
+
--- ywq/wwwroot/js/homework9.9.js
+++ ywq/wwwroot/js/homework9.9.js
@@ -1,44 +1,44 @@
 ﻿//使用“模拟名称空间”技术，构建一个函数函数yz.fei.get986(number)；
 //在yz.fei.get986(number)中包含不带参数的函数has9() / has8() / has6() ；
 //让get986()通过调用has9()/has8()/has6()，能找出number以内有多少个数字包含：9或者8或6。
-var global = {};
-global.yz = {};
-global.yz.fei = {};
-global.yz.fei.get986 = function () { };
-global.yz.fei.get986 = function (number) {
-    var result = 0;
-    for (var i = 0; i < number + 1; i++) {
-        if (has9(i) || has8(i) || has6(i)) {
-            result++;
-        } else {
-            //什么都不做
-        }
-    }
-    console.log(result);
-};
+//var global = {};
+//global.yz = {};
+//global.yz.fei = {};
+//global.yz.fei.get986 = function () { };
+//global.yz.fei.get986 = function (number) {
+//    var result = 0;
+//    for (var i = 0; i < number + 1; i++) {
+//        if (has9(i) || has8(i) || has6(i)) {
+//            result++;
+//        } else {
+//            //什么都不做
+//        }
+//    }
+//    console.log(result);
+//};
 
 
-var get986 = function () {
-    var result = 0;
-    for (var i = 0; i < number + 1; i++) {
-        if (has9() || has8() || has6()) {
-            result++;
-        } else {
-            //什么都不做
-        }
-    }
+//var get986 = function () {
+//    var result = 0;
+//    for (var i = 0; i < number + 1; i++) {
+//        if (has9() || has8() || has6()) {
+//            result++;
+//        } else {
+//            //什么都不做
+//        }
+//    }
     
-    function has9() {
-        return hasX(i, 9);
-    }
-    function has8() {
-        return hasX(i, 9);
-    }
-    function has6() {
-        return hasX(i, 6);
-    }
-    console.log(result);
-};
+//    function has9() {
+//        return hasX(i, 9);
+//    }
+//    function has8() {
+//        return hasX(i, 9);
+//    }
+//    function has6() {
+//        return hasX(i, 6);
+//    }
+//    console.log(result);
+//};
 
 
 
@@ -54,30 +54,30 @@ var get986 = function () {
  * has9(909) => true
  * has9(9月9) =>false
  */
-function has9(number) {
-    return hasX(number, 9);
-}
+//function has9(number) {
+//    return hasX(number, 9);
+//}
 
-function has8(number) {
-    return hasX(number, 8);
-}
+//function has8(number) {
+//    return hasX(number, 8);
+//}
 
-function has6(number) {
-    return hasX(number, 6);
-}
+//function has6(number) {
+//    return hasX(number, 6);
+//}
 
 
 
-function hasX(number, x) {
-    if (typeof (number) === 0 && number > 0 && String(number)[1] != ".") {
-        var parameter = String(number);
-        var find = String(x);
-        return parameter.indexOf(find) >= 0;
-    } else {
-        console.log("请输入正整数")；
-    }
+//function hasX(number, x) {
+//    if (typeof (number) === 0 && number > 0 && String(number)[1] != ".") {
+//        var parameter = String(number);
+//        var find = String(x);
+//        return parameter.indexOf(find) >= 0;
+//    } else {
+//        console.log("请输入正整数");
+//    }
     
-}
+//}
 
 //使用上述函数，找出10000以内有多少个数字包含：9或者8或者6。
 /*测试
@@ -86,15 +86,15 @@ function hasX(number, x) {
  * 输入has（18）=>5
 */
 
-function has(number) {
-    var result = 0;
-    for (var i = 0; i < number + 1; i++) {
-        if (has9(i) || has8(i) || has6(i)) {
-            result++;
-        } else {
-            //什么都不做
-        }
-    }
-    console.log(result);
-}
-has(20);
+//function has(number) {
+//    var result = 0;
+//    for (var i = 0; i < number + 1; i++) {
+//        if (has9(i) || has8(i) || has6(i)) {
+//            result++;
+//        } else {
+//            //什么都不做
+//        }
+//    }
+//    console.log(result);
+//}
+//has(20);
