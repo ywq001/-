6af869df142b47d7bbfb23706905d16febcc5caf
Administrator <Administrator@5I1ZFT9ZO2GAJV7>
2019-09-17 22:04:14 +0800

    完成求助页面以及统计有多少个悬赏大于1的求助

--- ywq/wwwroot/js/homework9.13.js
+++ ywq/wwwroot/js/homework9.13.js
@@ -13,6 +13,17 @@ setInterval(changeColor, 1000);
 //模拟求助首页，并：
 
 //统计有多少个悬赏大于1的求助
+var joy = document.getElementsByClassName('beauty');
+var counter = 0;
+for (var i = 0; i < joy.length; i++) {
+    if (joy[i].innertext > 0) {
+        counter++;
+    } else {
+        //no
+    }
+}
+alert(counter);
 //将状态为“协助中”的求助背景改成灰黑色
+
 //写一个函数，可以统计某个求助使用了多少关键字
 //如果总结数为0，将其从DOM树中删除
