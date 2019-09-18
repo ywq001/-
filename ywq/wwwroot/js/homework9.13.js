878859009d8d539b603f13579efd674294d57438
Administrator <Administrator@5I1ZFT9ZO2GAJV7>
2019-09-18 20:29:56 +0800

    完成”将状态为“协助中”的求助背景改成灰黑色“以及”写一个函数，可以统计某个求助使用了多少关键字“

--- ywq/wwwroot/js/homework9.13.js
+++ ywq/wwwroot/js/homework9.13.js
@@ -16,7 +16,7 @@ setInterval(changeColor, 1000);
 var joy = document.getElementsByClassName('beauty');
 var counter = 0;
 for (var i = 0; i < joy.length; i++) {
-    if (joy[i].innertext > 0) {
+    if (+joy[i].innerText > 1) {
         counter++;
     } else {
         //no
@@ -24,6 +24,25 @@ for (var i = 0; i < joy.length; i++) {
 }
 alert(counter);
 //将状态为“协助中”的求助背景改成灰黑色
+function help() {
+    var assist = document.getElementsByClassName('idcard');
+    for (var i = 0; i < assist.length; i++) {
+        if (assist[i].innerText === '协助中') {
+            assist[i].style.backgroundColor = '#33333378';
+        } else {
+            //跳过
+        }
+    }
+}
+help();   
 
 //写一个函数，可以统计某个求助使用了多少关键字
+function keyWord() {
+    var fields = document.getElementsByClassName('gotta');
+    for (var i = 0; i < fields.length; i++) {
+        console.log(fields[0].getElementsByTagName('a').length);
+    }
+    
+}
+keyWord();
 //如果总结数为0，将其从DOM树中删除
