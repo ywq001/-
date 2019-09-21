400852ec647faad35d33a8f2f804bc36954fdfac
Administrator <Administrator@5I1ZFT9ZO2GAJV7>
2019-09-21 22:00:54 +0800

    完成参考求助首页侧边栏关键字，实现：当鼠标移动到关键字上，显示关键字被使用次数

--- ywq/wwwroot/js/homework9.13.js
+++ ywq/wwwroot/js/homework9.13.js
@@ -1,66 +1,66 @@
 ﻿
 //实现铃铛（没有学bootstrap的同学用文字代替）闪烁效果
-function changeColor() {
-    var star = document.getElementById('flicker');
-    if (star.style.color === 'red') {
-        star.style.color = 'green';
-    } else {
-        star.style.color = 'red';
-    }
-}
-setInterval(changeColor, 1000);
+//function changeColor() {
+//    var star = document.getElementById('flicker');
+//    if (star.style.color === 'red') {
+//        star.style.color = 'green';
+//    } else {
+//        star.style.color = 'red';
+//    }
+//}
+//setInterval(changeColor, 1000);
 
 //模拟求助首页，并：
 
 //统计有多少个悬赏大于1的求助
-var joy = document.getElementsByClassName('beauty');
-var counter = 0;
-for (var i = 0; i < joy.length; i++) {
-    if (+joy[i].innerText > 1) {
-        counter++;
-    } else {
-        //no
-    }
-}
-alert(counter);
+//var joy = document.getElementsByClassName('beauty');
+//var counter = 0;
+//for (var i = 0; i < joy.length; i++) {
+//    if (+joy[i].innerText > 1) {
+//        counter++;
+//    } else {
+//        //no
+//    }
+//}
+//alert(counter);
 //将状态为“协助中”的求助背景改成灰黑色
-function help() {
-    var assist = document.getElementsByClassName('idcard');
-    for (var i = 0; i < assist.length; i++) {
-        if (assist[i].innerText === '协助中') {
-            assist[i].style.backgroundColor = '#33333378';
-        } else {
-            //跳过
-        }
-    }
-}
-help();
+//function help() {
+//    var assist = document.getElementsByClassName('idcard');
+//    for (var i = 0; i < assist.length; i++) {
+//        if (assist[i].innerText === '协助中') {
+//            assist[i].style.backgroundColor = '#33333378';
+//        } else {
+//            //跳过
+//        }
+//    }
+//}
+//help();
 
 //写一个函数，可以统计某个求助使用了多少关键字
-function keyWord() {
-    var fields = document.getElementsByClassName('gotta');
-    for (var i = 0; i < fields.length; i++) {
-        console.log(fields[0].getElementsByTagName('a').length);
-    }
+//function keyWord() {
+//    var fields = document.getElementsByClassName('gotta');
+//    for (var i = 0; i < fields.length; i++) {
+//        console.log(fields[0].getElementsByTagName('a').length);
+//    }
 
-}
-keyWord();
+//}
+//keyWord();
 //如果总结数为0，将其从DOM树中删除
-function removeChild() {
-    var summarize = document.querySelectorAll("[ywq-summarize]");
-    for (var j = 0; j < summarize.length; j++) {
-        var career = summarize[j].getElementsByClassName('bear');
-        for (var i = 0; i < career.length; i++) {
-            if (career[i].getElementsByTagName('span')[2].innerText === '0') {
-                summarize[j].removeChild(summarize[j].childNodes[5]);
-            } else {
-                //跳过
-            }
-        }
-    }
+//function removeChild() {
+//    var summarize = document.querySelectorAll("[ywq-summarize]");
+//    for (var j = 0; j < summarize.length; j++) {
+//        var career = summarize[j].getElementsByClassName('bear');
+//        for (var i = 0; i < career.length; i++) {
+//            if (career[i].getElementsByTagName('span')[2].innerText === '0') {
+//                summarize[j].removeChild(summarize[j].childNodes[5]);
+//            } else {
+//                //跳过
+//            }
+//        }
+//    }
 
-}
-removeChild();
+//}
+//removeChild();
 
 //参考用户注册页面，创建一下函数：
 
@@ -68,13 +68,28 @@ removeChild();
 
 
 
-console.log(document.querySelector("[ywq-password]").length)
+//console.log(document.querySelector("[ywq-password]").length);
 
 //如果密码和确认密码不一致，弹出提示
-var password = document.querySelector("[ywq-password]");
-var confirmPassword = document.querySelector("[ywq-verify-password]");
-if (password === confirmPassword) {
-    //继续
-} else {
-    alert("确认密码与密码不一致")；
+//var password = document.querySelector("[ywq-password]");
+//var confirmPassword = document.querySelector("[ywq-verify-password]");
+//if (password === confirmPassword) {
+//    //继续
+//} else {
+//    alert("确认密码与密码不一致");
+//}
+
+
+//参考求助首页侧边栏关键字，实现：当鼠标移动到关键字上，显示关键字被使用次数
+var antistop = document.querySelectorAll("[ywq-mouse-event]");
+for (var i = 0; i < antistop.length; i++) {
+
+    antistop[i].onmouseover = function (event) {
+
+        console.log(this.getAttribute('data-original-title'));
+
+    };
 }
+
+
+
