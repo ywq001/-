cdb554be97b0f86c8ed892b73f9479ecfaa5ebd5
Administrator <Administrator@5I1ZFT9ZO2GAJV7>
2019-09-23 21:54:10 +0800

    参考消息页面：完成勾选全选功能

--- ywq/wwwroot/js/homework9.16.js
+++ ywq/wwwroot/js/homework9.16.js
@@ -1,4 +1,22 @@
-﻿//参考一起帮首页，实现右侧漂浮并底对齐左侧文本框的效果
-window.addEventListener('scroll', function () {
-    console.log(this.document.body.scrollTop);
-});
+﻿//参考消息页面：完成勾选全选功能
+//document.querySelector("[ywq-check-all]").addEventListener('click', function () {
+//    var allFunctions = document.querySelectorAll("[ywq-check]");
+//    if (document.querySelector("[ywq-check-all]").checked) {
+//        for (var i = 0; i < allFunctions.length; i++) {
+//            allFunctions[i] = true;
+//        }
+//    } else {
+//        for (var j = 0; j < allFunctions.length; j++) {
+//            allFunctions[j] = false;
+//        }
+//    }
+//});
+
+
+var selectAll = document.querySelector("[ywq-check-all]");
+selectAll.onclick = function () {
+    let select = document.querySelectorAll("[ywq-check]");
+    for (var i = 0; i < select.length; i++) {
+        select[i].checked = selectAll.checked;
+    }
+};
