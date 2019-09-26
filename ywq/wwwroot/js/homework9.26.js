fb4d9b54dff4de7d9bc456815c11fffb543f1539
Administrator <Administrator@5I1ZFT9ZO2GAJV7>
2019-09-26 20:50:06 +0800

    完成按自己的情况，生成一个JSON字符串，包括真实姓名、QQ昵称、年龄、性别、兴趣爱好、自我介绍……，

---
+++ ywq/wwwroot/js/homework9.26.js
@@ -0,0 +1,13 @@
+﻿//JSON生成和解析：
+//按自己的情况，生成一个JSON字符串，包括真实姓名、QQ昵称、年龄、性别、兴趣爱好、自我介绍……，
+
+var introduce = { name: '于维谦', age: 21, QQ: 839152795, hobby: ['basketball', 'online game'], gender: 'male' };
+console.log(JSON.stringify(introduce));
+console.log(JSON.stringify(introduce, ['age']));
+console.log(JSON.stringify(introduce, null, ' '));
+console.log(JSON.stringify(introduce, function (key, value) {
+    if (typeof value === 'string') {
+        return value.toUpperCase();
+    }
+    return value;
+}));
