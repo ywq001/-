914d2017fcdefe7cfd3b8d0e536c22833b16ab67
86182 <839152795@qq.com>
2019-10-10 20:32:31 +0800

    打地鼠作业修改

---
+++ ywq/wwwroot/js/js作业.js
@@ -0,0 +1,13 @@
+﻿//2.已有如下代码：
+var sname = "飞哥";
+var a = {
+    sname: '老程',
+    fn: function () {
+        console.log(this.sname);
+    }
+}
+//请用不同的方式调用fn()函数，能分别打印出‘飞哥’和‘老程’
+a.fn.call();
+var bname = { sname: "飞哥" };
+a.fn.apply(bname);
+a.fn();
