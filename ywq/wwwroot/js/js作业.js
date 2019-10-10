c3199d2c7cf3bd8b01d1aa607248d492e63e6933
86182 <839152795@qq.com>
2019-10-10 20:55:22 +0800

    this（总结）和箭头函数作业

--- ywq/wwwroot/js/js作业.js
+++ ywq/wwwroot/js/js作业.js
@@ -11,3 +11,33 @@ a.fn.call();
 var bname = { sname: "飞哥" };
 a.fn.apply(bname);
 a.fn();
+
+
+//不改变上述代码，分别使用call()和apply() ，打印出‘文轩’和‘两开花’
+
+var sname = "飞哥";
+
+var a = {
+    sname: '老程',
+    fn: function () {
+        console.log(this.sname);
+    }
+}
+
+a.fn.call({ sname: "文轩" });
+var bname = { sname: "两开花" };
+a.fn.apply(bname);
+
+
+//将fn()永久拷贝到showName() ，始终打印‘源栈最棒！^ _ ^’
+
+var sname = "飞哥";
+
+var a = {
+    sname: '老程',
+    fn: function () {
+        console.log(this.sname);
+    }
+}
+
+var show = a.fn.bind({ sname: "源栈最棒！^ _ ^" });
