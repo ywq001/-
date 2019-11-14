a055ab749a938100ed8a493773707d5dc5f2b04e
86182 <839152795@qq.com>
2019-11-14 18:04:35 +0800

    修改猜数字

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -276,91 +276,99 @@ namespace CSharp
             //GetMax(scoers);
 
             //计算得到源栈同学的平均成绩（精确到两位小数），方法名GetAverage()
-            static void GetAverage(double[] ary)
-            {
-                double sum = 0;
-                for (int i = 0; i < ary.Length; i++)
-                {
-                    sum += ary[i];
+            //static void GetAverage(double[] ary)
+            //{
+            //    double sum = 0;
+            //    for (int i = 0; i < ary.Length; i++)
+            //    {
+            //        sum += ary[i];
 
-                }
-                Console.WriteLine(Math.Round(sum / ary.Length,2));
-            }
-            double[] scoers = new double[] { 68.5, 67.5, 96.5, 87.5, 56.5, 80 };
-            GetAverage(scoers);
+            //    }
+            //    Console.WriteLine(Math.Round(sum / ary.Length, 2));
+            //}
+            //double[] scoers = new double[] { 68.5, 67.5, 96.5, 87.5, 56.5, 80 };
+            //GetAverage(scoers);
 
             //完成“猜数字”游戏，方法名GuessMe()
 
-            //static void GuessMe(int min, int max)
-            //{
-            //    int number;
-            //    int guessNumbers;
-            //    Random rd = new Random();
-            //    number = rd.Next(min, max);
-            //    int count = 0;
-            //    Console.WriteLine("请输入小于"+max+"大于"+min+"的猜测数值：");
-            //    guessNumbers = Convert.ToInt32(Console.ReadLine());
-            //    while (guessNumbers <= max)
-            //    {
-            //        if (number == guessNumbers)
-            //        {
-            //            if (count<=9)
-            //            {
-            //                if (count<=8)
-            //                {
-            //                    if (count<=5)
-            //                    {
-            //                        Console.WriteLine("你真牛逼");
-            //                        break;
-            //                    }
-            //                    else
-            //                    {
-            //                        Console.WriteLine("不错嘛");
-            //                    }
-            //                }
-            //                else
-            //                {
-            //                    Console.WriteLine("还可以");
-            //                    break;
-            //                }
-            //            }
-            //            else
-            //            {
-            //                Console.WriteLine("还行");
-            //                break;
-            //            }
-                        
-            //        }
-            //        else
-            //        {
-            //            count++;
-            //            if (count == 10)
-            //            {
-            //                Console.WriteLine("(～￣(OO)￣)ブ");
-            //                break;
-            //            }
+            static void GuessMe(int min, int max)
+            {
+                int number;
+                int guessNumbers;
+                Random rd = new Random();
+                number = rd.Next(min, max);
+                int count = 1;
+                Console.WriteLine("请输入小于" + max + "大于" + min + "的猜测数值：");
+                guessNumbers = Convert.ToInt32(Console.ReadLine());
+                while (count < 11)
+                {
+                    if (guessNumbers.GetType() == typeof(int))
+                    {
+                        if (number == guessNumbers)
+                        {
+                            if (count <= 9)
+                            {
+                                if (count <= 8)
+                                {
+                                    if (count <= 5)
+                                    {
+                                        Console.WriteLine("你真牛逼");
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("不错嘛");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("还可以");
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("还行");
+                                break;
+                            }
 
-            //            else
-            //            {
-            //                if (guessNumbers<number)
-            //                {
-            //                    Console.WriteLine("小了");
-            //                }
-            //                else
-            //                {
-            //                    Console.WriteLine("大了");
-            //                }
-            //            }
-            //        }
-            //        Console.WriteLine("请再次输入猜测的数字：");
-            //        guessNumbers = Convert.ToInt32(Console.ReadLine());
-            //    }
-                
-            //}
-            ////test1=>当五次内猜对的时候的显示你真牛逼，然后跳出循环，小于8次猜对输出不错嘛，然后跳出循环，9次猜对输出还可以，10次猜对输出还行
-            ////test2=>当猜错时猜的数字比随机数小的时候输出小了然后继续输入猜测的数字，比随机数大就输出大了，然后继续输入
-            ////test3=>当第10次还没有猜对时，输出(～￣(OO)￣)ブ，跳出循环游戏结束
-            //GuessMe(1,1000);
+                        }
+                        else
+                        {
+                            
+                            if (count == 10)
+                            {
+                                Console.WriteLine("(～￣(OO)￣)ブ");
+                                break;
+                            }
+
+                            else
+                            {
+                                if (guessNumbers < number)
+                                {
+                                    Console.WriteLine("小了");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("大了");
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("请输入正整数");
+                    }
+                    count++;
+                    Console.WriteLine("请再次输入猜测的数字：");
+                    guessNumbers = Convert.ToInt32(Console.ReadLine());
+                }
+
+            }
+            //test1=>当五次内猜对的时候的显示你真牛逼，然后跳出循环，小于8次猜对输出不错嘛，然后跳出循环，9次猜对输出还可以，10次猜对输出还行
+            //test2=>当猜错时猜的数字比随机数小的时候输出小了然后继续输入猜测的数字，比随机数大就输出大了，然后继续输入
+            //test3=>当第10次还没有猜对时，输出(～￣(OO)￣)ブ，跳出循环游戏结束
+            GuessMe(1, 1000);
         }
     }
 }
