adeb93ece642487ea764916f75fbdf6f34fd08ae
86182 <839152795@qq.com>
2019-12-18 17:10:28 +0800

    完成声明一个委托

---
+++ CSharp/OtherAssignments/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.OtherAssignments
+{
+    public class Person
+    {
+        public int age = 18;
+    }
+
+    //声明一个委托：打水（ProvideWater），可以接受一个Person类的参数，返回值为int类型
+    public delegate int ProvideWater(Person person);
+
+
+}
