516de772f68a935178b9189e03b1d9794206ba73
86182 <839152795@qq.com>
2019-12-23 17:18:18 +0800

    完成生成XML的users对象

--- CSharp/OtherAssignments/Program.cs
+++ CSharp/OtherAssignments/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 using System.Xml;
+using CSharp.OtherAssignments;
 
 namespace CSharp
 {
@@ -96,72 +97,62 @@ namespace CSharp
             ///使用分号（;）或者换行进行了分隔。
             ///请删除其中重复的email地址，并按每30个email一行（行内用; 分隔）重新组织
 
-
-
             //按以下格式生成一个XML对象
-            XElement articles = new XElement(
-                "articles",
-                new XComment("以下存放所有“源栈”所有文章"),
-                new XElement("article",
-                    new XAttribute("isDraft",false),
-                    new XElement("id","1"),
-                    new XElement("title", "源栈培训：C#进阶-7：Linq to XML"),
-                    new XElement("body", "什么是XML（EXtensible Markup Language）" +
-                    "是一种文本文件格式被设计用来传输和存储数据由：标签和属性组成元素（节点），由元素组成“树状结构”必须有而且只能有一个根节点其他："),
-                    new XElement("authorId","1"),
-                    new XElement("publishDate", "2019/6/18 18:15"),
-                    new XElement("comments",
-                    new XElement("comment",
-                    new XElement("id","12"),
-                    new XElement("body", "不错，赞！"),
-                    new XElement("authorId","2"),
-                    new XAttribute("recommend",true)),
-                    new XElement("comment",
-                    new XElement("id","14"),
-                    new XElement("body", "作业太难了"),
-                    new XElement("authorId","3")))),
-                new XElement("article",
-                    new XElement("id","2"),
-                    new XElement("title", "源栈培训：C#进阶-6：异步和并行"),
-                    new XElement("authorId",1),
-                    new XAttribute("isDraft",true))
-                );
-            Console.WriteLine(articles);
 
-            //然后，将其以文件形式存放到磁盘中；
-            XDocument article = new XDocument(
-                new XDeclaration("1.0", "utf-8", "yes"),
-                articles);
-            article.Save("E:\\新建文件夹\\article.xml");
+            //Console.WriteLine(XmlHomework.xmlarticles());
 
-            //再从磁盘中读取到内存中
-            XElement element = XElement.Load("E:\\新建文件夹\\article.xml");
 
-            //在根节点下添加一个新的article元素，内容至少包含id、title和authorId
-            articles.Add(new XElement("article",
-                         new XElement("id", 1),
-                         new XElement("title", "net"),
-                         new XElement("authorId", "2")));
-            articles.Save("E:\\article.xml");
-            Console.WriteLine(articles);
 
-            //删除id = 12的评论
-            XElement idCard = (from a in articles.Descendants("comment")
-                               where a.Element("id").Value == "12"
-                               select a).Single();
-            idCard.Remove();
-            articles.Save("D:\\article.xml");
-            Console.WriteLine(articles);
-            //articles.Descendants("id").Where(x => (string)x == "12");
+            ////然后，将其以文件形式存放到磁盘中；
+            //XDocument article = new XDocument(
+            //    new XDeclaration("1.0", "utf-8", "yes"),
+            //    XmlHomework.xmlarticles());
+            //article.Save("E:\\新建文件夹\\article.xml");
 
-            //改变id = 2的article：isDraft = false，title = 源栈培训：C#进阶-8：异步和并行
-            XElement idCard2 = (from a in articles.Descendants("article")
-                                where a.Element("id").Value == "2"
-                                select a).Single();
-            idCard2.SetAttributeValue("isDraft", false);
-            idCard2.SetElementValue("title", "源栈培训：C#进阶-8：异步和并行");
-            articles.Save("E:\\Temp\\article.xml");
-            Console.WriteLine(articles);
+            ////再从磁盘中读取到内存中
+            //XElement element = XElement.Load("E:\\新建文件夹\\article.xml");
+
+            ////在根节点下添加一个新的article元素，内容至少包含id、title和authorId
+            //XmlHomework.xmlarticles().Add(new XElement("article",
+            //             new XElement("id", 1),
+            //             new XElement("title", "net"),
+            //             new XElement("authorId", "2")));
+            //XmlHomework.xmlarticles().Save("E:\\article.xml");
+            //Console.WriteLine(XmlHomework.xmlarticles());
+
+            ////删除id = 12的评论
+            //XElement idCard = (from a in XmlHomework.xmlarticles().Descendants("comment")
+            //                   where a.Element("id").Value == "12"
+            //                   select a).Single();
+            //idCard.Remove();
+            ////articles.Save("D:\\article.xml");
+            //Console.WriteLine(XmlHomework.xmlarticles());
+            ////articles.Descendants("id").Where(x => (string)x == "12");
+
+            ////改变id = 2的article：isDraft = false，title = 源栈培训：C#进阶-8：异步和并行
+            //XElement idCard2 = (from a in XmlHomework.xmlarticles().Descendants("article")
+            //                    where a.Element("id").Value == "2"
+            //                    select a).Single();
+            //idCard2.SetAttributeValue("isDraft", false);
+            //idCard2.SetElementValue("title", "源栈培训：C#进阶-8：异步和并行");
+            //XmlHomework.xmlarticles().Save("E:\\Temp\\article.xml");
+            //Console.WriteLine(XmlHomework.xmlarticles());
+
+            //参照上述articles，代码生成一个XML的users对象，能够存放用户的id、name和password，然后并存放到磁盘
+            Console.WriteLine(XmlHomework.xmlusers());
+
+            //XDocument user = new XDocument(
+            //    new XDeclaration("1.0", "utf-8", "yes"),
+            //    XmlHomework.xmlusers());
+            //XmlHomework.xmlusers().Save("E:\\Temp\\users.xml");
+
+            ///扩展user和articles的内容，使其能够完成以下操作：
+            ///根据用户名查找他发布的全部文章
+            ///统计出每个用户各发表了多少篇文章
+            ///查出每个用户最近发布的一篇文章
+            ///每个用户评论最多的一篇文章
+            ///删除没有发表文章的用户
+            
         }
 
         
---
+++ CSharp/OtherAssignments/XmlHomework.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace CSharp.OtherAssignments
+{
+    class XmlHomework
+    {
+      static XElement articles;
+        static XElement users;
+      public  static XElement xmlarticles()
+        {
+            articles = new XElement(
+            "articles",
+            new XComment("以下存放所有“源栈”所有文章"),
+            new XElement("article",
+                new XAttribute("isDraft", false),
+                new XElement("id", "1"),
+                new XElement("title", "源栈培训：C#进阶-7：Linq to XML"),
+                new XElement("body", "什么是XML（EXtensible Markup Language）" +
+                "是一种文本文件格式被设计用来传输和存储数据由：标签和属性组成元素（节点），由元素组成“树状结构”必须有而且只能有一个根节点其他："),
+                new XElement("authorId", "1"),
+                new XElement("publishDate", "2019/6/18 18:15"),
+                new XElement("comments",
+                    new XElement("comment",
+                        new XElement("id", "12"),
+                        new XElement("body", "不错，赞！"),
+                        new XElement("authorId", "2"),
+                        new XAttribute("recommend", true)),
+                    new XElement("comment",
+                        new XElement("id", "14"),
+                        new XElement("body", "作业太难了"),
+                        new XElement("authorId", "3")))),
+            new XElement("article",
+                new XElement("id", "2"),
+                new XElement("title", "源栈培训：C#进阶-6：异步和并行"),
+                new XElement("authorId", 1),
+                new XAttribute("isDraft", true))
+            );
+            return articles;
+        }
+
+      public static XElement xmlusers()
+        {
+            users = new XElement(
+                "users",
+                new XElement("user",
+                    new XAttribute("userId","1"),
+                    new XElement("name","飞哥"),
+                    new XElement("password","123456")),
+                new XElement("user",
+                    new XAttribute("userId","2"),
+                    new XElement("name","陈百万"),
+                    new XElement("passwprd","123456")),
+                new XElement("user",
+                    new XElement("userId","3"),
+                    new XElement("name","阿泰"),
+                    new XElement("password","123456"))
+                );
+            return users;
+        }
+
+    }
+}
