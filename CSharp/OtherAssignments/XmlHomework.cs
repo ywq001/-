7de317dd3cfb088afe9a0ad54c074a11e568c2d6
86182 <839152795@qq.com>
2020-01-02 21:27:23 +0800

    删除没有发表文章的用户

--- CSharp/OtherAssignments/XmlHomework.cs
+++ CSharp/OtherAssignments/XmlHomework.cs
@@ -9,92 +9,93 @@ namespace CSharp.OtherAssignments
 {
     class XmlHomework
     {
-      static XElement articles;
-        static XElement users;
+
       public  static XElement xmlarticles()
         {
-            articles = new XElement(
+          XElement  Getarticles = new XElement(
             "articles",
             new XComment("以下存放所有“源栈”所有文章"),
             new XElement("article",
                 new XAttribute("isDraft", false),
                 new XElement("authorName","飞哥"),
-                new XElement("id", "1"),
+                new XElement("id", 1),
                 new XElement("title", "源栈培训：C#进阶-7：Linq to XML"),
                 new XElement("body", "什么是XML（EXtensible Markup Language）" +
                 "是一种文本文件格式被设计用来传输和存储数据由：标签和属性组成元素（节点），由元素组成“树状结构”必须有而且只能有一个根节点其他："),
-                new XElement("authorId", "1"),
+                new XElement("authorId", 1),
                 new XElement("publishDate", "2019/6/18 18:15"),
                 new XElement("comments",
                     new XElement("comment",
-                        new XElement("id", "12"),
+                        new XElement("id", 12),
                         new XElement("body", "不错，赞！"),
-                        new XElement("authorId", "2"),
+                        new XElement("authorId", 2),
                         new XElement("authorName","陈百万"),
                         new XAttribute("recommend", true)),
                     new XElement("comment",
-                        new XElement("id", "14"),
+                        new XElement("id", 14),
                         new XElement("body", "作业太难了"),
-                        new XElement("authorId", "3"),
+                        new XElement("authorId", 3),
                         new XElement("authorName","阿泰")))),
             new XElement("article",
-                new XElement("id", "2"),
+                new XElement("id", 2),
                 new XElement("authorName", "飞哥"),
                 new XElement("title", "源栈培训：C#进阶-6：异步和并行"),
                 new XElement("authorId", 1),
                 new XElement("publishDate", "2019/6/15 18:15"),
                 new XElement("comments",
                     new XElement("comment",
-                        new XElement("id", "12"),
+                        new XElement("id", 12),
                         new XElement("body", "不错，赞！"),
-                        new XElement("authorId", "2"),
+                        new XElement("authorId", 2),
                         new XElement("authorName", "陈百万"),
                         new XAttribute("recommend", true))),
                 new XAttribute("isDraft", true)),
             new XElement("article",
-                new XElement("id", "2"),
+                new XElement("id", 2),
                 new XElement("authorName", "陈百万"),
                 new XElement("title", "源栈培训：C#"),
                 new XElement("authorId", 2),
                 new XElement("publishDate", "2019/5/18 18:15"),
                 new XElement("comments",
                     new XElement("comment",
-                        new XElement("id", "12"),
+                        new XElement("id", 12),
                         new XElement("body", "不错，赞！"),
-                        new XElement("authorId", "2"),
+                        new XElement("authorId", 2),
                         new XElement("authorName", "阿泰"),
                         new XAttribute("recommend", true))),
                 new XAttribute("isDraft", true)),
             new XElement("article",
-                new XElement("id", "2"),
+                new XElement("id", 2),
                 new XElement("authorName", "陈百万"),
                 new XElement("title", "源栈培训：sql"),
                 new XElement("authorId", 2),
                 new XElement("publishDate", "2019/5/18 18:15"),
                 new XAttribute("isDraft", true))
             );
-            return articles;
+            return Getarticles;
         }
 
       public static XElement xmlusers()
         {
-            users = new XElement(
+           XElement Getusers = new XElement(
                 "users",
                 new XElement("user",
-                    new XAttribute("userId","1"),
+                    new XElement("userId",1),
                     new XElement("name","飞哥"),
                     new XElement("password","123456")),
                 new XElement("user",
-                    new XAttribute("userId","2"),
+                    new XElement("userId",2),
                     new XElement("name","陈百万"),
                     new XElement("passwprd","123456")),
                 new XElement("user",
-                    new XElement("userId","3"),
+                    new XElement("userId",3),
                     new XElement("name","阿泰"),
                     new XElement("password","123456"))
                 );
-            return users;
+            return Getusers;
         }
+        static XElement articles = XElement.Load("E:\\新建文件夹\\a.xml");
+        static XElement users = XElement.Load("E:\\新建文件夹\\user.xml");
 
         ///根据用户名查找他发布的全部文章
         public static void findArticle()
@@ -147,6 +148,19 @@ namespace CSharp.OtherAssignments
             {
                 Console.WriteLine(item);
             }
+        }
+
+        //删除没有发表文章的用户
+        public static void deleteUser()
+        {
+            var user = from u in users.Descendants("user")
+                       join a in xmlarticles().Descendants("article")
+                       on u.Element("userId").Value equals a.Element("authorId").Value into ja
+                       from result in ja.DefaultIfEmpty()
+                       where result == null
+                       select u;
+            user.Remove();
+            users.Save("E:\\新建文件夹\\U.xml");
         }
     }
 }
