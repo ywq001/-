0c4a9605470994d16b593d132a0d7f7a867a53d4
86182 <839152795@qq.com>
2020-01-04 20:35:12 +0800

    将生成验证码的代码拆分成若干个方法

--- Draw/VerificationCode .cs
+++ Draw/VerificationCode .cs
@@ -14,21 +14,40 @@ namespace Draw
         ///随机字符串
         ///混淆用的各色像素点
         ///混淆用的直线（或曲线）
+         static Random random = new Random();
+        static string getRandom = string.Empty;
+        static Bitmap image = new Bitmap(200, 200);
+        static Graphics g = Graphics.FromImage(image);
         public static void GetVerificationCode()
         {
+           
+            g.Clear(Color.AliceBlue);
+            PointF pointF = new PointF(50, 60);
+            lineation();
             
+            g.DrawString(
+                RandomChar(),
+                new Font("宋体", 30),
+                new SolidBrush(Color.DarkRed),
+                pointF);
+            
+            
+            image.Save(@"E:\新建文件夹\验证码.jpg", ImageFormat.Jpeg);
+        }
+
+        public static string RandomChar()
+        {
             string[] randomCharacter = { "1", "2", "3", "4", "a", "b", "e", "f", "g", "h", "l" };
-            string getRandom = string.Empty;
-            Random random = new Random();
             for (int i = 0; i < 4; i++)
             {
                 int randomIndex = random.Next(11);
                 getRandom += randomCharacter[randomIndex];
             }
-            Bitmap image = new Bitmap(200, 200);
-            Graphics g = Graphics.FromImage(image);
-            g.Clear(Color.AliceBlue);
-            PointF pointF = new PointF(50, 60);
+            return getRandom;
+        }
+
+        public static void lineation()
+        {
             for (int i = 0; i < 10; i++)
             {
                 int x = random.Next(0, 180);
@@ -37,11 +56,10 @@ namespace Draw
                 g.DrawLine(new Pen(Color.Blue), new Point(x, y), new Point(y, z));
                 g.DrawLine(new Pen(Color.Yellow), new Point(x, z), new Point(z, y));
             }
-            g.DrawString(
-                getRandom,
-                new Font("宋体", 30),
-                new SolidBrush(Color.DarkRed),
-                pointF);
+        }
+
+        public static void GetRandomPixel()
+        {
             for (int i = 0; i < 100; i++)
             {
                 int X = random.Next(0, 190);
@@ -49,8 +67,6 @@ namespace Draw
                 image.SetPixel(X, Y, Color.Red);
                 image.SetPixel(Y, X, Color.Yellow);
             }
-            
-            image.Save(@"E:\新建文件夹\验证码.jpg", ImageFormat.Jpeg);
         }
     }
 }
